0d6c3a97765ee39a67ec7242d262356e83060ec2
Rodrigo Weber Sesterheim <rsesterheim@maisaedu.com.br>
2024-09-21 16:23:28 -0300

    feat: use trycatch statement

--- src/use-cases/user/_register-user.use-case.js
+++ src/use-cases/user/_register-user.use-case.js
@@ -5,8 +5,13 @@ import { userService } from '../../services/index.js';
 
 function _validateUserBody (user) {
   const schema = Joi.object({
+    profile_photo: Joi.string().uri(),
+    name: Joi.string().min(3).required(),
+    phone: Joi.string().min(8).required(),
     email: Joi.string().email().required(),
     password: Joi.string().min(6).required(),
+    document: Joi.string().min(11).required(),
+    age: Joi.number().integer().min(0).required(),
   });
 
   const { error } = schema.validate(user);
@@ -22,7 +27,16 @@ export async function registerUser (user) {
   const hashedPassword = await bcrypt.hash(user.password, 10);
 
   user.password = hashedPassword;
-  const userRegistered = await userService.register(user);
 
-  return userRegistered;
+  try {
+    const userRegistered = await userService.register(user);
+
+    return userRegistered;
+  } catch (error) {
+    if (error.message.includes('duplicate key value violates unique constraint')) {
+      throw new Error('User already exists');
+    }
+
+    throw new Error(error.message);
+  }
 };
