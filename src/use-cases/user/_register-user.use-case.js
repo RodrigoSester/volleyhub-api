7e64df816dea99ecbd0564443a1fbeaad43e4ead
Rodrigo Weber Sesterheim <rsesterheim@maisaedu.com.br>
2024-09-20 21:16:41 -0300

    feat: validate user body

--- src/use-cases/user/_register-user.use-case.js
+++ src/use-cases/user/_register-user.use-case.js
@@ -1,6 +1,22 @@
+import Joi from 'joi';
 import bcrypt from 'bcrypt';
 
+function _validateUserBody (user) {
+  const schema = Joi.object({
+    email: Joi.string().email().required(),
+    password: Joi.string().min(6).required(),
+  });
+
+  const { error } = schema.validate(user);
+
+  if (error) {
+    throw new Error(error.message);
+  }
+};
+
 export async function registerUser (user) {
+  _validateUserBody(user);
+
   const hashedPassword = await bcrypt.hash(user.password, 10);
 
   user.password = hashedPassword;
