302d186505653e6b4acb0a594bb6c9d0c795992d
Rodrigo Weber Sesterheim <rsesterheim@maisaedu.com.br>
2024-09-20 22:08:35 -0300

    feat: user's migration

--- database/migrations/20240921010416_create_user.js
+++ database/migrations/20240921010416_create_user.js
@@ -2,14 +2,26 @@
  * @param { import("knex").Knex } knex
  * @returns { Promise<void> }
  */
-exports.up = function(knex) {
-  
+export function up(knex) {
+  return knex.schema.createTable('user', (table) => {
+    table.increments('id').primary();
+
+    table.string('name').notNullable();
+    table.string('email').notNullable();
+    table.string('password').notNullable();
+    table.string('document').notNullable();
+    table.string('phone').notNullable();
+    table.string('profile_photo').notNullable();
+    table.integer('age').notNullable();
+
+    table.timestamp('created_at').defaultTo(knex.fn.now());
+  });
 };
 
 /**
  * @param { import("knex").Knex } knex
  * @returns { Promise<void> }
  */
-exports.down = function(knex) {
-  
+export function down(knex) {
+  return knex.schema.dropTable('user');
 };
