7f7ea83bf074fd79634038a77df0e583bcb9be6b
Rodrigo Weber Sesterheim <rsesterheim@maisaedu.com.br>
2024-09-20 22:29:47 -0300

    feat: export connection

--- knexfile.js
+++ knexfile.js
@@ -1,3 +1,8 @@
 import { developmentConfig } from "./database/config/index.js";
 
+import knex from 'knex';
+
 export const development = developmentConfig;
+
+const db = knex(development);
+export default db;
