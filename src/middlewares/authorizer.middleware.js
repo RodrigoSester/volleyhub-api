3967295b77e4008a81adf94173d2bd83129a52ca
Rodrigo Weber Sesterheim <rsesterheim@maisaedu.com.br>
2024-09-24 17:39:24 -0300

    feat: apply authorizer middleware in all routes

---
+++ src/middlewares/authorizer.middleware.js
@@ -0,0 +1,24 @@
+import dotenv from "dotenv";
+import jsonwebtoken from "jsonwebtoken";
+
+function _verifyAuthorization(authorization) {
+  if (!authorization) {
+    throw new Error("Unauthorized");
+  }
+
+  jsonwebtoken.verify(authorization, dotenv.config().parsed.JWT_SECRET, (err) => {
+    if (err) {
+      throw new Error("Unauthorized");
+    }
+  });
+}
+
+export function authorizer(req, res, next) {
+  const { authorization } = req.headers;
+
+  _verifyAuthorization(authorization);
+
+  req.authorizer = jsonwebtoken.decode(authorization);
+
+  next();
+}
--- src/routes/index.js
+++ src/routes/index.js
@@ -1,13 +1,14 @@
 import { Router } from "express";
 
 import authRoutes from "./_auth.routes.js";
+import { authorizer } from "../middlewares/authorizer.middleware.js";
 import teamsRouter from "./_teams.routes.js";
 import userRoutes from "./_user.routes.js";
 
 const router = Router();
 
-router.use("/auth", authRoutes);
-router.use("/user", userRoutes);
-router.use("/teams", teamsRouter);
+router.use("/auth", authorizer, authRoutes);
+router.use("/user", authorizer, userRoutes);
+router.use("/teams", authorizer, teamsRouter);
 
 export default router;
