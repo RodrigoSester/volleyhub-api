61d493e676ba7ad6c36e51b3b56ea8393b1c9920
Rodrigo Weber Sesterheim <rsesterheim@maisaedu.com.br>
2024-09-28 18:34:52 -0300

    feat: implement query to get player's data

--- src/services/_team-player.service.js
+++ src/services/_team-player.service.js
@@ -10,9 +10,18 @@ export async function getAll() {
 
 export async function getById(playerId) {
   return await db()
-    .select('*')
-    .from('team_players')
-    .where({ 'player_id': playerId });
+    .select('tp.id', 'tp.team_id', 'tp.player_id', 'tp.is_active', 'tp.type', 'tp.shirt_number')
+    .from('team_players as tp')
+    .innerJoin('users as u', 'tp.player_id', 'u.id')
+    .select(
+      'u.name as player_name', 
+      'u.email as player_email', 
+      'u.phone as player_phone', 
+      'u.profile_photo as player_photo', 
+      'u.document as player_document'
+    )
+    .where({ 'player_id': playerId })
+    .first();
 }
 
 export async function edit(playerDTO) {
