07b1d3e242d9b10311023d8426e3473f8f995c45
Rodrigo Weber Sesterheim <rsesterheim@maisaedu.com.br>
2024-09-28 18:21:16 -0300

    fix: fix service's name

--- src/use-cases/team/player/_get-all-players.use-case.js
+++ src/use-cases/team/player/_get-all-players.use-case.js
@@ -4,11 +4,11 @@ import {
 } from '../../../services/index.js';
 
 export async function getAllPlayers(teamPlayersDTO) {
-  const team = await teamsService.getById(teamPlayersDTO.teamId);
+  const team = await teamService.getById(teamPlayersDTO.teamId);
 
   if (!team) {
     throw new Error('Team not found');
   }
 
-  return await teamPlayersService.getAll();
+  return await teamPlayerService.getAll();
 }
