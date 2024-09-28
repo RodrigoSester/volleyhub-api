d0c033a5221930c7464607ec5c37316bd3ca4c62
Rodrigo Weber Sesterheim <rsesterheim@maisaedu.com.br>
2024-09-28 19:03:32 -0300

    feat: validate body

--- src/use-cases/team/player/_edit-player.use-case.js
+++ src/use-cases/team/player/_edit-player.use-case.js
@@ -3,7 +3,28 @@ import {
   teamPlayerService,
 } from '../../../services/index.js';
 
+import Joi from 'joi';
+
+function _validateUpdateTeamPlayerBody(team) {
+  const schema = Joi.object({
+    teamId: Joi.number().integer().required(),
+    playerId: Joi.number().integer().required(),
+    shirt_number: Joi.number().integer().required(),
+    is_active: Joi.boolean().required(),
+    type: Joi.string().valid('player', 'coach', 'assistant', 'owner').required(),
+    user_id: Joi.number().integer().required(),
+  });
+  
+  const { error } = schema.validate(team);
+  
+  if (error) {
+    throw new Error(error.message);
+  }
+}
+
 export async function editPlayer(playerDTO) {
+  _validateUpdateTeamPlayerBody(playerDTO);
+
   const team = await teamService.getById(playerDTO.teamId);
 
   if (!team) {
