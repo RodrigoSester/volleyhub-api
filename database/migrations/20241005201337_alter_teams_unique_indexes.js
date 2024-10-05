18590099fa780764b4e2f1bb1381913dd2c2b596
Rodrigo Weber Sesterheim <rsesterheim@maisaedu.com.br>
2024-10-05 17:31:02 -0300

    fix: fix indexes and unique constraints table's team

---
+++ database/migrations/20241005201337_alter_teams_unique_indexes.js
@@ -0,0 +1,31 @@
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+export function up(knex) {
+  return knex.schema.raw(`
+    ALTER TABLE teams DROP CONSTRAINT IF EXISTS uq_team_modality_created_by;
+
+    DROP INDEX IF EXISTS idx_team_modality_created_by;
+    DROP INDEX IF EXISTS uq_teams_modality_created_by;
+
+    CREATE UNIQUE INDEX idx_team_modality_created_by ON teams (modality, created_by) WHERE NOT is_deleted;
+    ALTER TABLE teams ADD CONSTRAINT uq_teams_modality_created_by UNIQUE (modality, created_by, is_deleted);
+  `);
+};
+
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+export function down(knex) {
+  return knex.schema.raw(`
+    ALTER TABLE teams DROP CONSTRAINT IF EXISTS uq_teams_modality_created_by;
+    DROP INDEX IF EXISTS idx_team_modality_created_by;
+
+    CREATE UNIQUE INDEX uq_teams_modality_created_by ON teams (modality, created_by);
+    CREATE UNIQUE INDEX idx_team_modality_created_by ON teams (modality, created_by);
+    
+    ALTER TABLE teams ADD CONSTRAINT uq_team_modality_created_by UNIQUE (modality, created_by);
+  `);
+};
