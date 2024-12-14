698cb37dc1eb3b3ddc3d660ca76754c341445b43
Rodrigo Weber Sesterheim <rsesterheim@maisaedu.com.br>
2024-12-14 17:56:36 -0300

    feat(database-config): force password string

--- database/config/index.js
+++ database/config/index.js
@@ -8,7 +8,7 @@ const config = {
     port: process.env.DB_PORT,
     host: process.env.DB_HOST,
     user: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
+    password: String(process.env.DB_PASSWORD),
     database: process.env.DB_NAME,
   },
   migrations: {
