fa9c1c8c40f10d66aca0e1a9bdaefe6fe0c3b69f
Rodrigo Weber Sesterheim <rsesterheim@maisaedu.com.br>
2025-01-12 20:12:17 -0300

    feat(matches): create matches table

--- database/migrations/20241129061427_create_matches.js.js
+++ database/migrations/20241129061427_create_matches.js.js
@@ -3,7 +3,29 @@
  * @returns { Promise<void> }
  */
 export function up(knex) {
-  // return knex.schema('matches').createTable('matches', (table) => {});
+  return knex.schema.createTable("matches", (table) => {
+    table.increments("id").primary();
+
+    table.integer("team_home_id").notNullable();
+    table.foreign("team_home_id").references("id").inTable("teams");
+
+    table.integer("team_away_id").notNullable();
+    table.foreign("team_away_id").references("id").inTable("teams");
+
+    table.enum("modality", ["female", "male", "mixed"]).notNullable();
+
+    table.date("date").notNullable();
+    table.integer("value").notNullable();
+
+    table.integer("created_by").references("id").inTable("users");
+    table.timestamp("created_at").defaultTo(knex.fn.now());
+    table.integer("updated_by").references("id").inTable("users");
+    table.timestamp("updated_at").defaultTo(knex.fn.now());
+    table.boolean("is_deleted").defaultTo(false);
+    table.timestamp("deleted_at");
+
+    table.index(["team_home_id", "team_away_id"], "idx_matches_team_home_id_team_away_id");
+  });
 };
 
 /**
@@ -11,5 +33,5 @@ export function up(knex) {
  * @returns { Promise<void> }
  */
 export function down(knex) {
-  // return knex.schema('matches').dropTable();
+  return knex.dropTable("matches");
 };
