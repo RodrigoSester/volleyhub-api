65059b9424cbe4ebc27ac1d04e9c1830ad168e6d
Rodrigo Weber Sesterheim <rsesterheim@maisaedu.com.br>
2025-06-14 19:28:45 -0300

    refactor(matches): convert exports to ES6 syntax in migration file

--- database/migrations/20250614222617_alter_matches.js
+++ database/migrations/20250614222617_alter_matches.js
@@ -1,18 +1,10 @@
-/**
- * @param { import("knex").Knex } knex
- * @returns { Promise<void> }
- */
-exports.up = function(knex) {
+export function up(knex) {
   return knex.schema.alterTable("matches", (table) => {
     table.integer("team_away_id").nullable().alter();
   });
 };
 
-/**
- * @param { import("knex").Knex } knex
- * @returns { Promise<void> }
- */
-exports.down = function(knex) {
+export function down(knex) {
   return knex.schema.alterTable("matches", (table) => {
     table.integer("team_away_id").notNullable().alter();
   });
